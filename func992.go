669abdc785c3ba5ab3fdaa631cde3a724ebacaff
Youngs羊示 <youngsdream@qq.com>
2026-01-21 23:39:07 +0800

    feat:df

--- func992.go
+++ func992.go
@@ -5,11 +5,30 @@ import (
 )
 
 func main()  {
+	var fs = [4]func(){}
 
-	for i:=0;i<3;i++{
+
+	for i:=0;i<4;i++{
+		defer fmt.Println("defer i",i)
 		defer func (){
-			fmt.Println("defer func",i)
+			fmt.Println("defer clouser i",i)
 		}()
+		fs[i] = func() {fmt.Println(" clouser i",i)}
+	}
+	for _,f :=range fs{
+		f()
 	}
 
 }
+//  clouser i 0
+//  clouser i 1
+//  clouser i 2
+//  clouser i 3
+// defer clouser i 3
+// defer i 3
+// defer clouser i 2
+// defer i 2
+// defer clouser i 1
+// defer i 1
+// defer clouser i 0
+// defer i 0
